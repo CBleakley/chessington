f0fc55978c11a81f4ff4a2576770f4f819ce5c2f
Conor Bleakley <Conor.Bleakley@softwire.com>
2024-06-26 17:35:32 +0100

    passing test 3

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import player from "../player";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,15 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+
+        let validMoves: Square[] = [];
+        let currentSquare: Square = board.findPiece(this);
+
+        for(let i: number = 0; i < 8; i++) {
+            if (i !== currentSquare.col) validMoves.push(new Square(currentSquare.row, i));
+            if (i !== currentSquare.row) validMoves.push(new Square(i, currentSquare.col));
+        }
+
+        return validMoves;
     }
 }
