e5d6016273caf762d5b20504bcfbcbec109e2eda
Conor Bleakley <Conor.Bleakley@softwire.com>
2024-06-26 16:55:03 +0100

    improved variable names and removed redundant comments

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -10,19 +10,17 @@ export default class Pawn extends Piece {
 
     public getAvailableMoves(board: Board) {
 
-        let moveArr: Square[] = []; // Array of squares to move to
-        let currSquare: Square = board.findPiece(this);
+        let validMoves: Square[] = [];
+        let currentSquare: Square = board.findPiece(this);
 
         if (this.player === Player.WHITE) {
-            // White moves up, row incremented
-            let nextSquare: Square = new Square(currSquare.row + 1, currSquare.col);
-            moveArr.push(nextSquare);
+            let possibleNextSquare: Square = new Square(currentSquare.row + 1, currentSquare.col);
+            validMoves.push(possibleNextSquare);
         } else if (this.player === Player.BLACK) {
-            // Black moves down, row decremented
-            let nextSquare: Square = new Square(currSquare.row - 1, currSquare.col);
-            moveArr.push(nextSquare);
+            let possibleNextSquare: Square = new Square(currentSquare.row - 1, currentSquare.col);
+            validMoves.push(possibleNextSquare);
         }
 
-        return moveArr;
+        return validMoves;
     }
 }
