4c97504f1a09911931239d4b00abd5db520da81e
Conor Bleakley <Conor.Bleakley@softwire.com>
2024-06-26 16:51:15 +0100

    rebasing test-2 onto master

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from '../square';
+import player from "../player";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -12,14 +13,25 @@ export default class Pawn extends Piece {
 
         let validMoves: Square[] = [];
         let currentSquare: Square = board.findPiece(this);
+        let possibleNextSquare: Square;
+        let startingRow: number;
+        let pieceDirection: number; // positive for moving up board, negative for moving down
 
-        if (this.player === Player.WHITE) {
-            let possibleNextSquare: Square = new Square(currentSquare.row + 1, currentSquare.col);
-            validMoves.push(possibleNextSquare);
-        } else if (this.player === Player.BLACK) {
-            let possibleNextSquare: Square = new Square(currentSquare.row - 1, currentSquare.col);
+        if(this.player === player.WHITE) {
+            startingRow = 1;
+            pieceDirection = 1;
+        }
+        else {
+            startingRow = 6;
+            pieceDirection = -1;
+        }
+
+        if (currentSquare.row == startingRow){
+            possibleNextSquare = new Square(currentSquare.row + (2 * pieceDirection), currentSquare.col);
             validMoves.push(possibleNextSquare);
         }
+        possibleNextSquare = new Square(currentSquare.row + (1 * pieceDirection), currentSquare.col);
+        validMoves.push(possibleNextSquare);
 
         return validMoves;
     }
