2c9088e5739090c7c66ed8c3d067c9c66173d7f7
Conor Bleakley <Conor.Bleakley@softwire.com>
2024-06-26 16:17:46 +0100

    passing test 1

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,20 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+
+        let moveArr: Square[] = []; // Array of squares to move to
+        let currSquare: Square = board.findPiece(this);
+
+        if (this.player === Player.WHITE) {
+            // White moves up, row incremented
+            let nextSquare: Square = new Square(currSquare.row + 1, currSquare.col);
+            moveArr.push(nextSquare);
+        } else if (this.player === Player.BLACK) {
+            // Black moves down, row decremented
+            let nextSquare: Square = new Square(currSquare.row - 1, currSquare.col);
+            moveArr.push(nextSquare);
+        }
+
+        return moveArr;
     }
 }
