c1a6054d524606c04476c4cd26bd22a3015597e5
Conor Bleakley <Conor.Bleakley@softwire.com>
2024-06-27 12:30:27 +0100

    Implemented basic knight movement. (passing test 6)

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,15 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let validMoves: Square[] = [];
+        let currentSquare: Square = board.findPiece(this);
+        let arrayOfDisplacements: number[][] = [    [2, 1], [2, -1], [-2, 1], [-2, -1],
+                                                    [1, 2], [-1, 2], [1, -2], [-1, -2]  ];
+
+        for (let i = 0; i < arrayOfDisplacements.length; i++) {
+            validMoves.push(new Square(currentSquare.row + arrayOfDisplacements[i][0], currentSquare.col + arrayOfDisplacements[i][1]));
+        }
+
+        return validMoves;
     }
 }
