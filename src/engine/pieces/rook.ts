02a93b9c947904d3ed043827508afcb8db163e48
Conor Bleakley <Conor.Bleakley@softwire.com>
2024-06-27 12:06:36 +0100

    Removed redundant imports

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,8 +1,6 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
-import Square from "../square";
-import player from "../player";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
