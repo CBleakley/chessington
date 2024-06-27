0879a75f21524708fbda17d08df5f86b8bf1710f
Conor Bleakley <Conor.Bleakley@softwire.com>
2024-06-27 11:28:26 +0100

    implemeted basic bishop movement, passing test 4

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,23 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+
+        let currentSquare: Square = board.findPiece(this);
+        let diagonalSquares: Square[] = [];
+        let currentRow = currentSquare.row;
+        let currentCol = currentSquare.col;
+        let displacement;
+
+        for (let i = 0; i <8; i++){
+            displacement = Math.abs(currentRow - i);
+            if(displacement === 0) continue;
+            diagonalSquares.push(new Square(i, currentCol - displacement));
+            diagonalSquares.push(new Square(i, currentCol + displacement));
+        }
+
+        const validMoves = diagonalSquares.filter(square => board.isSquareOnBoard(square));
+
+        return validMoves;
     }
+
 }
