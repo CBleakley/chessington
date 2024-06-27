f5b0dec69f75bde52f1c32e2d291f742887aad4d
Materol <gaarapavel@gmail.com>
2024-06-27 14:08:56 +0100

    Implemented basic king movement. (passing test-7)

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,15 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let validMoves: Square[] = [];
+        let currentSquare: Square = board.findPiece(this);
+        let arrayOfDisplacements: number[][] = [    [0, 1], [0, -1], [1, 0], [-1, 0],
+                                                    [1, -1], [1, 1], [-1, 1], [-1, -1]  ];
+
+        for (let i = 0; i < arrayOfDisplacements.length; i++) {
+            validMoves.push(new Square(currentSquare.row + arrayOfDisplacements[i][0], currentSquare.col + arrayOfDisplacements[i][1]));
+        }
+
+        return validMoves;
     }
 }
