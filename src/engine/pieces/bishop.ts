0e49a0ba006038f1f166053732be1904ff42bce9
Materol <gaarapavel@gmail.com>
2024-06-27 16:31:42 +0100

    Added bishop not moving through pieces functionality (passing test 10)

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import GameSettings from "../gameSettings";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,7 +10,45 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return board.getDiagonalSquares(board.findPiece(this));
+        let diagonalSquares: Square[] = [];
+        let startingSquare = board.findPiece(this);
+        let currentRow= startingSquare.row;
+        let currentCol= startingSquare.col;
+        let pieceInPossibleSquare;
+
+        for(let col = currentCol - 1, row = currentRow - 1; col >= 0 && row >= 0; col--, row--) {
+            pieceInPossibleSquare = board.getPiece(new Square(row, col));
+            if(pieceInPossibleSquare !== undefined) {
+                break;
+            }
+            diagonalSquares.push(new Square(row, col));
+        }
+
+        for(let col = currentCol - 1, row = currentRow + 1; col >= 0 && row < GameSettings.BOARD_SIZE; col--, row++) {
+            pieceInPossibleSquare = board.getPiece(new Square(row, col));
+            if(pieceInPossibleSquare !== undefined) {
+                break;
+            }
+            diagonalSquares.push(new Square(row, col));
+        }
+
+        for(let col = currentCol + 1, row = currentRow - 1; col < GameSettings.BOARD_SIZE && row >= 0; col++, row--) {
+            pieceInPossibleSquare = board.getPiece(new Square(row, col));
+            if(pieceInPossibleSquare !== undefined) {
+                break;
+            }
+            diagonalSquares.push(new Square(row, col));
+        }
+
+        for(let col = currentCol + 1, row = currentRow + 1; col < GameSettings.BOARD_SIZE && row < GameSettings.BOARD_SIZE; col++, row++) {
+            pieceInPossibleSquare = board.getPiece(new Square(row, col));
+            if(pieceInPossibleSquare !== undefined) {
+                break;
+            }
+            diagonalSquares.push(new Square(row, col));
+        }
+
+        return diagonalSquares;
     }
 
 }
